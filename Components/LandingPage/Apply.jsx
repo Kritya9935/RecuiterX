8ff487e3b64019b75aa2cfb025fd161fd3fc63f7
Kritya9935 <kashishg832@gmail.com>
2024-09-21 18:04:26 +0530

    second commit

--- Components/LandingPage/Apply.jsx
+++ Components/LandingPage/Apply.jsx
@@ -15,7 +15,7 @@ function Apply() {
                                 <div className="job_search">
                                     <h1 className="child_div_apply mt-5"><GoArchive /></h1>
                                     <h4 className="child_div_apply">1.Search A Job</h4>
-                                    <p className="tex p-3">Text editing and text formatting  etc.), fonts, and sizes. It was developed to integrate the features included</p>
+                                    <p className="tex p-3">Text editing and text formatting  etc., fonts, and sizes. It was developed to integrate the features included</p>
                                 </div>
                             </div>
 
@@ -23,14 +23,14 @@ function Apply() {
                                 <div className="job_search">
                                     <h1 className="child_div_apply mt-5"><GoArchive /></h1>
                                     <h4 className="child_div_apply">1.Search A Job</h4>
-                                    <p className="tex p-3">Text editing and text formatting  etc.), fonts, and sizes. It was developed to integrate the features included</p>
+                                    <p className="tex p-3">Text editing and text formatting  etc., fonts, and sizes. It was developed to integrate the features included</p>
                                 </div>
                             </div>
                             <div className="col-sm-4 box_border">
                                 <div className="job_search">
                                     <h1 className="child_div_apply mt-5"><GoArchive /></h1>
                                     <h4 className="child_div_apply">1.Search A Job</h4>
-                                    <p className="tex p-3">Text editing and text formatting  etc.), fonts, and sizes. It was developed to integrate the features included</p>
+                                    <p className="tex p-3">Text editing and text formatting  etc., fonts, and sizes. It was developed to integrate the features included</p>
                                 </div>
                             </div> 
                         </div>
